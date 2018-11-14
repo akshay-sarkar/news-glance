10e6e8e0557c14498d6d9674f02478d486db2924
Akshay Sarkar <akshay.sarkar.dbit@gmail.com>
2018-11-14 13:02:00 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/news-glance/precache-manifest.5e4034f507b2b5319e87baeacf630c6a.js"
+  "/news-glance/precache-manifest.8b1bc71816d3c9552ee6f64ab3e7033d.js"
 );
 
 workbox.clientsClaim();
